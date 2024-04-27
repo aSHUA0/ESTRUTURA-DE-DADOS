bb062cb72fc72d2ec6d73a47026d46b4671f279f
Guilherme Tavares <136861352+aSHUA0@users.noreply.github.com>
2024-04-27 19:12:06 -0300

    Update banda.h

--- Estrutura de dados/banda.h
+++ Estrutura de dados/banda.h
@@ -1,1 +1,15 @@
+#ifndef BANDA_H
+#define BANDA_H
 
+typedef struct Artista Artista;
+
+void leitorArtistas(Artista artistas[], int *num_artistas);
+void mostrarLista(Artista artistas[], int num_lista);
+void inserirOrdenado(Artista artistas[], int *num_artistas, Artista novo);
+void removerArtista(Artista artistas[], int *num_artistas, const char *remove_name);
+void editarArtista(Artista artistas[], int num_artistas, const char *edit_name);
+void buscaSequencial(Artista artistas[], int num_artistas);
+int buscaBinaria(Artista artistas[], int num_artistas, const char *search_name);
+void menu();
+
+#endif
