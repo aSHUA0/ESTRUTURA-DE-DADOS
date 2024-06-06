d33863884e83d9a8af817bb3c88bf69718c8a64b
Guilherme Tavares <136861352+aSHUA0@users.noreply.github.com>
2024-06-06 02:04:33 -0300

    Create artista.h

---
+++ Atividade Avaliativa 2/artista.h
@@ -0,0 +1,19 @@
+#ifndef MUSICA_H
+#define MUSICA_H
+
+typedef struct Musica Musica;
+typedef struct ListaDuplamenteEncadiada;
+
+Musica* criaNo();
+void adicionarMusica();
+int removerNo();
+void mostarMusica();
+void mostrarMusicaOrdenada();
+Musica* buscar();
+void atualizarMusica();
+void salvarMusica();
+void inserindoMusica();
+void apagarMusica();
+void menu();
+
+#endif
